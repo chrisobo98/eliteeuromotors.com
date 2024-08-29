a3eca9cfa09f9449449793c9e6a363e55a2227ed
chrisobo98 <christopherbermudez98@gmail.com>
2024-08-29 14:12:57 -0400

    chore: blog and careers page tightening up

--- components/Blog/FeaturedPost.vue
+++ components/Blog/FeaturedPost.vue
@@ -1,15 +1,22 @@
 <template>
-  <div v-if="post" class="featured-post">
-    <NuxtImg loading="lazy" :src="post.image" alt="" class="rounded-lg" />
-    <div>
-      <p class="text-red-600 text-2xl font-bold">{{ post.category }}</p>
-      <h3 class="text-2xl">{{ post.title }}</h3>
-      <p>{{ post.excerpt }}</p>
-      <p class="author my-2">
-        <NuxtImg :src="post.authorImage" alt="" />
-        {{ post.author }} - {{ formattedDate }}
-      </p>
-    </div>
+  <div v-if="post">
+    <NuxtLink :href="`/blog/${post.slug}`" class="md:flex">
+      <NuxtImg
+        loading="lazy"
+        :src="post.image"
+        alt=""
+        class="object-cover h-48 w-96 rounded-lg md:mx-4 my-4"
+      />
+      <div>
+        <p class="text-red-600 text-2xl font-bold">{{ post.category }}</p>
+        <h3 class="text-2xl">{{ post.title }}</h3>
+        <p>{{ post.excerpt }}</p>
+        <p class="author my-2">
+          <NuxtImg :src="post.authorImage" alt="" />
+          {{ post.author }} - {{ formattedDate }}
+        </p>
+      </div>
+    </NuxtLink>
   </div>
 </template>
 
@@ -42,21 +49,14 @@ const formattedDate = computed(() => {
 
 <style scoped>
 .featured-post {
-  display: flex;
   align-items: center;
-}
-.featured-post img {
-  width: 150px;
-  height: 150px;
-  object-fit: cover;
-  margin-right: 20px;
 }
 .author {
   display: flex;
   align-items: center;
 }
 .author img {
-  width: 40px;
+  width: auto;
   height: 40px;
   border-radius: 50%;
   margin-right: 10px;
